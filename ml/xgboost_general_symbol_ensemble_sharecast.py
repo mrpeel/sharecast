a2c3f8c40fc6e166e643d0560d3f061d962ae55f
mrpeel <mrpeel@gmail.com>
2018-01-31 20:00:42 +1100

    Change tree_method to prevent crashes

--- ml/xgboost_general_symbol_ensemble_sharecast.py
+++ ml/xgboost_general_symbol_ensemble_sharecast.py
@@ -471,11 +471,13 @@ def execute_preprocessor(transform_df, scaler, ce):
 def train_general_model(df_all_train_x, df_all_train_y, df_all_test_actuals, df_all_test_y, df_all_test_x, keras_models):
     #Train general model
     models = {}
-    print('Training xgboost log of y model...')
-    tree_method = 'hist'
+
+    tree_method = 'auto'
+    nthread = -1
     # Create model
-    models['log_y'] = xgb.XGBRegressor(nthread=-1, tree_method=tree_method, n_estimators=500, max_depth=70, base_score=0.1,
-                                       colsample_bylevel=0.7, colsample_bytree=1.0, gamma=0, learning_rate=0.025,
+    models['log_y'] = xgb.XGBRegressor(nthread=nthread, tree_method=tree_method,
+                                       n_estimators=1000, max_depth=70, base_score=0.1,
+                                       colsample_bylevel=0.7, colsample_bytree=1.0, gamma=0, learning_rate=0.05,
                                        min_child_weight=3)
 
     all_train_y = df_all_train_y.values
@@ -486,9 +488,7 @@ def train_general_model(df_all_train_x, df_all_train_y, df_all_test_actuals, df_
     all_test_x = df_all_test_x.values
     all_test_log_y = safe_log(all_test_y)
 
-    mae_vals_train = keras_models['mae_intermediate_model'].predict(all_train_x)
-    mae_vals_test = keras_models['mae_intermediate_model'].predict(all_test_x)
-
+    print('Training xgboost log of y model...')
     x_train, x_test, y_train, y_test = train_test_split(all_train_x, all_train_y, test_size = 0.15)
 
     eval_set = [(x_test, y_test)]
@@ -510,9 +510,12 @@ def train_general_model(df_all_train_x, df_all_train_y, df_all_test_actuals, df_
                 }
     })
 
+    print('Retrieving keras intermediate model vals...')
+    mae_vals_train = keras_models['mae_intermediate_model'].predict(all_train_x)
+    mae_vals_test = keras_models['mae_intermediate_model'].predict(all_test_x)
 
     print('Training xgboost log of y with keras outputs model...')
-    models['keras_mae'] = xgb.XGBRegressor(nthread=-1, tree_method=tree_method, n_estimators=500, max_depth=70,
+    models['keras_mae'] = xgb.XGBRegressor(nthread=nthread, tree_method=tree_method, n_estimators=1000, max_depth=70,
                                            learning_rate=0.025, base_score=0.25, colsample_bylevel=0.4,
                                            colsample_bytree=0.55, gamma=0, min_child_weight=0)
 
@@ -536,7 +539,7 @@ def train_general_model(df_all_train_x, df_all_train_y, df_all_test_actuals, df_
         })
 
     print('Training xgboost log of log of y with keras outputs model...')
-    models['keras_log_mae'] = xgb.XGBRegressor(nthread=-1, tree_method=tree_method, n_estimators=500,
+    models['keras_log_mae'] = xgb.XGBRegressor(nthread=nthread, tree_method=tree_method, n_estimators=1000,
                                                max_depth=130,
                                                base_score=0.4,
                                                colsample_bylevel=0.4,
