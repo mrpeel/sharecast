c607a6ff8b143305de1553bb598c8ab767f3c1d3
mrpeel <mrpeel@gmail.com>
2018-05-24 06:01:19 +1000

    Add logging, ability to generate labels for data set and saftey nan values cleanup

--- ml/execute_xgboost_predictions.py
+++ ml/execute_xgboost_predictions.py
@@ -1,6 +1,8 @@
 from xgboost_general_symbol_ensemble_sharecast import *
 
 # @profile
+
+
 def prepare_data_for_model(share_data):
     # symbol_models = {}
     symbols = share_data['symbol'].unique()
@@ -18,7 +20,6 @@ def prepare_data_for_model(share_data):
     y_dfs = []
     actuals_dfs = []
 
-
     # prep data for fitting into both model types
     for symbol in symbols:
         gc.collect()
@@ -33,7 +34,8 @@ def prepare_data_for_model(share_data):
 
         y_dfs.append(model_data[LABEL_COLUMN + '_scaled'])
         actuals_dfs.append(model_data[LABEL_COLUMN])
-        x_dfs.append(model_data.drop([LABEL_COLUMN, LABEL_COLUMN + '_scaled'], axis=1))
+        x_dfs.append(model_data.drop(
+            [LABEL_COLUMN, LABEL_COLUMN + '_scaled'], axis=1))
 
     # Create concatenated dataframes with all data
     print('Creating concatenated dataframes')
@@ -52,14 +54,28 @@ def prepare_data_for_model(share_data):
 
     return df_all_x, df_all_y, df_all_actuals
 
+
 def main(run_config):
     # Prepare run_str
     run_str = datetime.now().strftime('%Y%m%d%H%M')
 
+    # Setup logging.
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%d/%m/%Y %I:%M:%S %p',
+        level=logging.DEBUG,
+        filename='logs/execution-' + run_str + '.log'
+    )
+
     print('Starting sharecast prediction:', run_str)
 
     # Load and divide data
-    share_data = load_data(run_config['data_file'])
+    if 'generate_labels' in run_config and run_config['generate_labels'] is True:
+        share_data = load_data(run_config['data_file'], run_config['generate_label_weeks'],
+                               run_config['reference_date'], run_config['label_file_name'])
+    else:
+        share_data = load_data(run_config['data_file'])
+
     gc.collect()
 
     # Divide data into symbols and general data for training an testing
@@ -85,7 +101,6 @@ def main(run_config):
     # Execute pre-processing
     df_all_x = execute_preprocessor(df_all_x, se, imputer, scaler)
 
-
     print('Loading keras models')
     # Load keras models
     keras_models = {
@@ -98,32 +113,40 @@ def main(run_config):
             'k_mae_mape': k_mae_mape,
         }),
         'mae_intermediate_model': load_model('models/keras-mae-intermediate-model.h5',
-        custom_objects={
-            'k_mean_absolute_percentage_error': k_mean_absolute_percentage_error,
-            'k_mae_mape': k_mae_mape,
-        }),
+                                             custom_objects={
+                                                 'k_mean_absolute_percentage_error': k_mean_absolute_percentage_error,
+                                                 'k_mae_mape': k_mae_mape,
+                                             }),
     }
 
     print('Loading xgboost model list')
     xgb_models = load_xgb_models()
-    predictions = execute_model_predictions(df_all_x, x_model_names, xgb_models, keras_models)
+    predictions = execute_model_predictions(
+        df_all_x, x_model_names, xgb_models, keras_models)
 
     print('Loading bagging models')
     bagging_model = load_model('models/keras-bagging-model.h5', custom_objects={
         'k_mean_absolute_percentage_error': k_mean_absolute_percentage_error,
         'k_mae_mape': k_mae_mape,
-        })
+    })
     bagging_scaler = load('models/deep-bagging-scaler.pkl.gz')
-    deep_bagged_predictions = execute_deep_bagging(bagging_model, bagging_scaler, predictions)
+    deep_bagged_predictions = execute_deep_bagging(
+        bagging_model, bagging_scaler, predictions)
     predictions['deep_bagged_predictions'] = deep_bagged_predictions
 
-    if 'eval_results' in run_config and run_config['eval_results'] == True:
+    if 'eval_results' in run_config and run_config['eval_results'] is True:
         assess_results(predictions, x_model_names, df_all_actuals, run_str)
+
+    print('Prediction completed')
 
 
 if __name__ == "__main__":
     run_config = {
-        'data_file': './data/data_with_labels.pkl.gz',
+        'data_file': './data/ml-20180512-labelled.pkl.gz',
+        'generate_labels': False,
+        'generate_label_weeks': 8,
+        'reference_date': '2018-05-12',
+        'label_file_name': '',
         'eval_results': True,
     }
 
